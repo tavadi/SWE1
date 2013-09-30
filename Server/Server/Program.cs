3bd7cd743af7555726cd88630f762632a1b59cb7
Daniel <if12b067@technikum-wien.at>
2013-09-30 19:51:56 +0200

    Client deleted - Client == Browser

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Net;
-using System.Net.Sockets;
+
 
 
 namespace Server
@@ -15,28 +13,12 @@ namespace Server
         static void Main(string[] args)
         {
 
-            TcpListener listener = new TcpListener(IPAddress.Any, 8080);
-            listener.Start();
+            Server WebServer = new Server();
 
-            Console.WriteLine("Server is running at port 8080 ...");
-            Console.WriteLine("Server is waiting for Connection ...");
-
-            Socket s = listener.AcceptSocket();
-            NetworkStream stream = new NetworkStream(s);
-            StreamReader sr = new StreamReader(stream);
-
-            Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
-
-            string line = sr.ReadLine();
-
-            while (line != "exit")
-            {
-                line = sr.ReadLine();
-                Console.WriteLine(line);
-            }
+            WebServer.StartServer();
 
 
-            Console.WriteLine("Connection close from " + s.RemoteEndPoint);
+            // Wait for Input
             Console.ReadLine();
         }
     }
---
+++ Server/Server/Server.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    public class Server
+    {
+
+        public void StartServer()
+        {
+
+            new Thread(StartServerThread).Start();
+
+            
+        }
+
+
+        // ServerThread ... listening the whole time
+        public void StartServerThread()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+            listener.Start();
+
+            Console.WriteLine("Server is running at port 8080 ...");
+            Console.WriteLine("Server is waiting for Connection ...");
+
+            while (true)
+            {
+                Socket s = listener.AcceptSocket();
+
+                ParameterizedThreadStart ThreadStart = new ParameterizedThreadStart(newClient);
+
+                Thread newClientThread = new Thread(newClient);
+                newClientThread.Start(s);
+            }
+        }
+
+
+
+        public void newClient(object SessionClient)
+        {
+
+            Socket s = (Socket)SessionClient;
+            NetworkStream stream = new NetworkStream(s);
+            StreamReader sr = new StreamReader(stream);
+
+            Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+
+    }
+}
