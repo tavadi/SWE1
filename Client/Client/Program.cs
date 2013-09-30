ed27b1ed830f8fbcbf225498192b6fb2b9e2aae8
Daniel <if12b067@technikum-wien.at>
2013-09-30 17:59:31 +0200

    Server and Client added

---
+++ Client/Client/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TcpClient client = new TcpClient();
+            client.Connect("localhost", 8080);
+
+            Console.WriteLine("Connectiong ...");
+
+            NetworkStream stream = client.GetStream();
+            StreamWriter sw = new StreamWriter(stream);
+
+            string input = "";
+
+            while (input != "exit")
+            {
+                input = Console.ReadLine();
+
+                sw.WriteLine("GET / HTTP/1.1");
+                sw.WriteLine("host: localhost");
+                sw.WriteLine("connection: close");
+                sw.WriteLine(input);
+                sw.WriteLine();
+                sw.Flush();
+            }
+
+
+            Console.WriteLine("Connection close ...");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+            listener.Start();
+
+            Console.WriteLine("Server is running at port 8080 ...");
+            Console.WriteLine("Server is waiting for Connection ...");
+
+            Socket s = listener.AcceptSocket();
+            NetworkStream stream = new NetworkStream(s);
+            StreamReader sr = new StreamReader(stream);
+
+            Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+            string line = sr.ReadLine();
+
+            while (line != "exit")
+            {
+                line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+
+
+            Console.WriteLine("Connection close from " + s.RemoteEndPoint);
+            Console.ReadLine();
+        }
+    }
+}
