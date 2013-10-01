070fdaa4ee494f29cb68c94578d45fb8c495f962
Daniel <if12b067@technikum-wien.at>
2013-10-01 10:09:40 +0200

    Client Backup -> Request ok, Reply fail

--- Server/Server/Server.cs
+++ Server/Server/Server.cs
@@ -7,18 +7,39 @@ using System.Threading;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Web;
+
 
 namespace Server
 {
     public class Server
     {
+        bool _ServerIsRunning;
 
+        // Konstruktor
+        public Server()     {    _ServerIsRunning = true;   }
+
+
+        // SETTER
+        public void setServerStatus(bool status)
+        {
+            _ServerIsRunning = status;
+        }
+
+        // GETTER
+        public bool getServerStatus()
+        {
+            return _ServerIsRunning;
+        }
+
+
+
+
+
+        // Create Server-Thread (MainThread) for listening
         public void StartServer()
         {
-
-            new Thread(StartServerThread).Start();
-
-            
+            new Thread(StartServerThread).Start();  
         }
 
 
@@ -31,34 +52,37 @@ namespace Server
             Console.WriteLine("Server is running at port 8080 ...");
             Console.WriteLine("Server is waiting for Connection ...");
 
+             
             while (true)
             {
+                // If Client connect --> new ClientThread
                 Socket s = listener.AcceptSocket();
 
-                ParameterizedThreadStart ThreadStart = new ParameterizedThreadStart(newClient);
-
-                Thread newClientThread = new Thread(newClient);
+                ParameterizedThreadStart ThreadStart = new ParameterizedThreadStart(ClientThread);
+                Thread newClientThread = new Thread(ClientThread);
                 newClientThread.Start(s);
             }
         }
 
 
 
-        public void newClient(object SessionClient)
+        public void ClientThread(object SessionClient)
         {
-
             Socket s = (Socket)SessionClient;
             NetworkStream stream = new NetworkStream(s);
             StreamReader sr = new StreamReader(stream);
-
+            StreamWriter sw = new StreamWriter(stream);
             Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
 
+            
             while (!sr.EndOfStream)
             {
                 string line = sr.ReadLine();
                 Console.WriteLine(line);
             }
-        }
+            
 
+            
+        }
     }
 }
---
+++ Server/Server/URL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Server
+{
+    public partial class URL
+    {
+        public string RetrievePassedUrl()
+        {
+            return HttpContext.Current.Server.UrlDecode(HttpContext.Current.Request.QueryString["url"]);
+        }
+    }
+}
